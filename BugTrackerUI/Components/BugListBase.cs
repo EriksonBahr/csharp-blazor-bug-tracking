22f938781aa5e5683297d539438b45e939aaa4d7
Erikson Bahr <erikson.bahr@hotmail.com>
2020-05-03 15:24:49 -0300

    Split the buglist component into two files: html/css and the code itself.

---
+++ BugTrackerUI/Components/BugListBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTrackerUI.Components
+{
+    public class BugListBase: ComponentBase
+    {
+        [Inject]
+        public IBugService BugService { get; set; }
+        public List<Bug> Bugs { get; set; }
+        protected override void OnInitialized()
+        {
+            Bugs = BugService.GetBugs().OrderByDescending(x => x.Priority).ToList();
+        }
+    }
+}
